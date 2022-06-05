2825c8996cfe0a48dbfc0b0cda2f9722cfbc2e97
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2022-06-05 18:31:06 +0200

    Completed median computer

--- exam-2020/ord2020/src/stuff/MedianComputer.java
+++ exam-2020/ord2020/src/stuff/MedianComputer.java
@@ -1,7 +1,31 @@
 package stuff;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MedianComputer {
 
-	// TODO: declare appropriate compute method
+	public static double compute(List<Double> nums) {
 
+		if (nums.size() == 0) throw new IllegalArgumentException();
+
+		nums = new ArrayList<>(nums);
+		Collections.sort(nums);
+		int len = nums.size();
+		
+		if (len % 2 == 1) {
+			return nums.get((len - 1) / 2);
+		} else {
+			int i = len / 2;
+			double lower = nums.get(i - 1);
+			double upper = nums.get(i);
+
+			return ( lower + upper ) / 2;
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
--- exam-2020/ord2020/src/stuff/MedianComputerTest.java
+++ exam-2020/ord2020/src/stuff/MedianComputerTest.java
@@ -1,6 +1,12 @@
 package stuff;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
 
@@ -9,19 +15,33 @@ public class MedianComputerTest {
 
 	// use as third argument to Assert.assertEquals(double, double, double) method for handling round-off errors
 	// e.q. Assert.assertEquals(6.0, MedianComputer.compute(...), roundErrorDelta);
-	double roundErrorDelta = 0.00000001;
+	double delta = 0.00000001;
 
-	@Test
-	public void testCompute1() {
-
-		// TODO: test relevant case 1
-		// Assert.assertEquals(...);
+	@Test(expected = IllegalArgumentException.class)
+	public void testEmpty() {
+		MedianComputer.compute(new ArrayList<Double>());
 	}
 
 	@Test
-	public void testCompute2() {
-		// TODO: test relevant case 2
+	public void testComputeOdd() {
+		List<Double> l = List.of(3., 2., 1.);
+		Assert.assertEquals(2, MedianComputer.compute(l), delta);
 	}
 
-	// TODO: more test methods
+	@Test
+	public void testComputeEven() {
+		List<Double> l = List.of(4., 3.);
+		Assert.assertEquals(3.5, MedianComputer.compute(l), delta);
+	}
+	
+	@Test
+	public void testMutable() {
+		List<Double> before = List.of(2., 3., 1.);
+		List<Double> l = new ArrayList<>(before);
+		MedianComputer.compute(l);
+		
+		// Order should stay unchanged
+		assertEquals(before, l);
+
+	}
 }
