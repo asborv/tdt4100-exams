a0a3b4b1f22874e3824690a179dfdbf4e94bec7c
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2022-06-06 17:17:06 +0200

    Completed part 10

--- kont-2021/src/test/java/del10/TestableClassTest.java
+++ kont-2021/src/test/java/del10/TestableClassTest.java
@@ -1,17 +1,22 @@
 package del10;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class TestableClassTest {
 
 	@Test
 	public void testIsMyStringEqualsToYieldsWrongResult() {
-
+		TestableClass tc = new TestableClass(0, "hello");
+		assertTrue(() -> tc.isMyStringEqualTo("HELLO".toLowerCase()));
 	}
 
 	@Test
 	public void testIncrementIntegerHandlesEdgeCases() {
-
+		TestableClass tc = new TestableClass((Integer.MAX_VALUE), "");
+		Long maxPlusOne = Long.valueOf(Integer.MAX_VALUE) + 1;
+		assertEquals(maxPlusOne, tc.getMyIntegerIncrement());
 	}
 }
