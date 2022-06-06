55f04248b5d7e70526f8c95a4b6534a775e87431
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2022-06-06 20:49:34 +0200

    Completed 3

--- kont-2020/src/main/java/stuff/MathController.java
+++ kont-2020/src/main/java/stuff/MathController.java
@@ -3,6 +3,7 @@ package stuff;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Random;
+import java.util.function.BinaryOperator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -15,17 +16,10 @@ public class MathController {
 	
 	Random rand;
 
-	@FXML
-	private ComboBox<Character> typeSelector;
-
-	@FXML
-	private TextField firstField;
-
-	@FXML
-	private TextField secondField;
-	
-	@FXML
-	private TextArea resultArea;
+	@FXML private ComboBox<Character> typeSelector;
+	@FXML private TextField firstField;
+	@FXML private TextField secondField;
+	@FXML private TextArea resultArea;
 
 	@FXML
 	private void initialize() {
@@ -45,8 +39,46 @@ public class MathController {
 	 */
 	@FXML
 	private void onCalculate() {
+
+		double n1, n2;
+
+		try {
+			n1 = Double.parseDouble(firstField.getText());
+			n2 = Double.parseDouble(secondField.getText());
+		} catch (NumberFormatException e) {
+			throw new IllegalStateException("Must calculate on numbers");
+		}
+
+		BinaryOperator<Integer> func2 = (x1, x2) -> x1 + x2;
+		BinaryOperator<Double> operator;
+		switch(typeSelector.getValue()) {
+			case '+':
+				operator = (a, b) -> a + b;
+				break;
+			case '-':
+				operator = (a, b) -> a - b;
+				break;
+			case '*':
+				operator = (a, b) -> a * b;
+				break;
+			case '/':
+				operator = (a, b) -> a / b;
+				break;
+			default:
+				throw new IllegalStateException("Operator must be one of: +-*/");
+		}
+
+		resultArea.setText(operator.apply(n1, n2).toString());
 	}
 
-	
-	// Is there a method missing here? Where could it be?
+	@FXML
+	private void randomizeNumbers() {
+		Random rand = new Random();
+
+		double n1 = rand.nextDouble() * 100;
+		double n2 = rand.nextDouble() * 100;
+
+		firstField.setText(String.valueOf(n1));
+		secondField.setText(String.valueOf(n2));
+	}
 }
