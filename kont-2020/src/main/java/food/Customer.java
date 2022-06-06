e8ee8c6db35bb266da626451166ac146e2dbaab5
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2022-06-06 21:04:07 +0200

    Completed 2.1

--- kont-2020/src/main/java/food/Customer.java
+++ kont-2020/src/main/java/food/Customer.java
@@ -1,14 +1,15 @@
 package food;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import food.def.ICustomer;
 
 public class Customer implements ICustomer {
 
-
-	// Add internal variables here:
-	
+	private String name;
+	private List<MealOrder> orders = new ArrayList<>();
 	
 	/**
 	 * Create a new customer
@@ -16,6 +17,8 @@ public class Customer implements ICustomer {
 	 * @param name The name of the customer
 	 */
 	public Customer(String name) {
+		if (name == null || name.isBlank()) throw new IllegalArgumentException("Name must be non-empty string");
+		this.name = name;
 	}
 	
 	
@@ -24,7 +27,7 @@ public class Customer implements ICustomer {
 	 * @return A list containing all meals bought by this customer
 	 */
 	public Collection<MealOrder> getMealsOrdered() {
-		return null; // Dummy return value
+		return new ArrayList<>(orders);
 	}
 
 	/**
@@ -35,15 +38,16 @@ public class Customer implements ICustomer {
 	 */
 	@Override
 	public void buyMeal(String meal, double price) {
+		MealOrder order = new MealOrder(meal, price);
+		orders.add(order);
 	}
-	
 	
 	/**
 	 * @return The number of meals ordered by this customer
 	 */
 	@Override
 	public int getNumberOfOrderedMeals() {
-		return 0; // Dummy return value
+		return orders.size();
 	}
 	
 	/**
@@ -51,7 +55,7 @@ public class Customer implements ICustomer {
 	 */
 	@Override
 	public String getName() {
-		return null; // Dummy return value
+		return name;
 	}
 
 	/**
@@ -59,7 +63,7 @@ public class Customer implements ICustomer {
 	 */
 	@Override
 	public String toString() {
-		return null; // dummy return value
+		return String.format("%s: %d", name, getNumberOfOrderedMeals());
 	}
 
 	/**
@@ -68,7 +72,9 @@ public class Customer implements ICustomer {
 	 */
 	@Override
 	public MealOrder getLastOrderedMeal() {
-		return null; // Dummy return value
+		return getNumberOfOrderedMeals() == 0
+			? null
+			: orders.get(getNumberOfOrderedMeals() - 1);
 	}
 	
 	/**
@@ -80,7 +86,10 @@ public class Customer implements ICustomer {
 	 */
 	@Override
 	public int timesEaten(String meal) {
-		return 0; // Dummy return value
+		// Assume safe cast of obvious reasons
+		return (int) orders.stream()
+								 			 .filter(order -> order.getName().equals(meal))
+								 			 .count();
 	}
 	
 	public static void main(String[] args) {
@@ -88,6 +97,6 @@ public class Customer implements ICustomer {
 		customer.buyMeal("pancakes", 100);
 		customer.buyMeal("pancakes", 75);
 		System.out.println("Skal være 2 kjøp: " + customer.getMealsOrdered().size());
-//		System.out.println("Skal være pris 75: " + customer.getLastOrderedMeal().getPrice()); // Som definert i README.
+		System.out.println("Skal være pris 75: " + customer.getLastOrderedMeal().getPrice()); // Som definert i README.
 	}
 }
--- kont-2020/src/main/java/food/MealOrder.java
+++ kont-2020/src/main/java/food/MealOrder.java
@@ -9,6 +9,22 @@ package food;
  */
 public class MealOrder {
 	
-	// Consult the README for a description of requirements.
-	
+	private String name;
+	private double price;
+
+	MealOrder(String name, double price) {
+		if (name == null || name.isBlank()) throw new IllegalArgumentException("Name must be non-empty string");
+		if (price < 0) throw new IllegalArgumentException("Price must be positive");
+
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
 }
